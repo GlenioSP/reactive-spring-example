0cd7eb2211ca2453e4a284a731862b054175bad9
gleniosp <glenio.apps@gmail.com>
2018-12-07 15:15:10 -0200

    Update to kotlin base

---
+++ ffc/src/main/kotlin/com/example/ffc/FfcApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ffc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FfcApplication
+
+fun main(args: Array<String>) {
+    runApplication<FfcApplication>(*args)
+}
